ee07081f371e0f1d4e4fe51c99bc9d70ba4a73d1
bambamhappydog <matjazsercer1@gmail.com>
2025-04-25 21:46:31 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+// Uporabnikov Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyD-LU4Vun6SKFSIhHEVquEkS-MStox9D84",
+  authDomain: "igre-registracija.firebaseapp.com",
+  projectId: "igre-registracija",
+  storageBucket: "igre-registracija.firebasestorage.app",
+  messagingSenderId: "365182438160",
+  appId: "1:365182438160:web:154b54a56501f54fbb3395",
+  measurementId: "G-32X03N3HR6"
+};
+firebase.initializeApp(firebaseConfig);
